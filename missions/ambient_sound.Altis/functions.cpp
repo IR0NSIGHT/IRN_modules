24b8611278162317a925681921b17ce289c54ef0
irnsght <irnsght@gmail.com>
2020-11-25 15:13:55 +0100

    changed description

--- missions/ambient_sound.Altis/functions.cpp
+++ missions/ambient_sound.Altis/functions.cpp
@@ -1,10 +1,16 @@
 class CfgFunctions
 {
-	class IRN
+	class IRN //my tag class
 	{
-        class IRN_ambientbattles {file = "WOLF_Modules\ambient_battle\ambient_battles.sqf";};
-	    class IRN_calcSoundPos {file = "WOLF_Modules\ambient_battle\IRN_calcSoundPos.sqf"};
-        class IRN_spawnSalvo {file = "WOLF_Modules\ambient_battle\IRN_spawnSalvo.sqf"};
-        class IRN_remoteSound {file = "WOLF_Modules\ambient_battle\IRN_remoteSound.sqf"};
+        class myCategory {
+            class calcSoundPos {file = "WOLF_Modules\ambient_battle\functions\fn_calcSoundPos.sqf";};
+			class testFunction  {file = "WOLF_Modules\ambient_battle\functions\fn_test.sqf";};
+            class spawnSalvo {file = "WOLF_Modules\ambient_battle\functions\fn_spawnSalvo.sqf"};
+			class spawnTracer {file = "WOLF_Modules\ambient_battle\functions\fn_spawnTracer.sqf"};
+			class randomVector {file = "WOLF_Modules\ambient_battle\functions\fn_randomVector.sqf"};
+			class explosionLight {file = "WOLF_Modules\ambient_battle\functions\fn_explosionLight.sqf"};
+			class killLights {file = "WOLF_Modules\ambient_battle\functions\fn_killLights.sqf"};
+			class interpolate {file = "WOLF_Modules\ambient_battle\functions\fn_interpolate.sqf"};
+        };
     };
 };
